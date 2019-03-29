dc21dabbc52a407f88795354bf9dcd9e9f59e65f
Big Mogician <sunaoran@youzan.com>
2019-03-29 14:40:07 +0800

    publish 2.1.2

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,12 +4,13 @@ import { defineUnit } from "./unit";
 
 defineUnit(["index", "Astroboy-router index.ts"], () => {
   it("test utils exports", () => {
-    expect(Object.keys(AST_ROUTER).length, "[utils] exports count").to.equal(7);
+    expect(Object.keys(AST_ROUTER).length, "[utils] exports count").to.equal(8);
     expect(typeof AST_ROUTER.CustomRoute, "[utils.CustomRoute] type is ").to.equal("function");
     expect(typeof AST_ROUTER.CustomPipe, "[utils.Index] type is ").to.equal("function");
     expect(typeof AST_ROUTER.FromBody, "[utils.FromBody] type is ").to.equal("function");
     expect(typeof AST_ROUTER.FromParams, "[utils.FromParams] type is ").to.equal("function");
     expect(typeof AST_ROUTER.FromQuery, "[utils.FromQuery] type is ").to.equal("function");
+    expect(typeof AST_ROUTER.FromRequest, "[utils.FromRequest] type is ").to.equal("function");
     expect(typeof AST_ROUTER.Router, "[utils.Router] type is ").to.equal("function");
     expect(typeof AST_ROUTER.createRouter, "[utils.createRouter] type is ").to.equal("function");
   });
